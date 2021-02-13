bbdcddc886f1d907476c35d3ac224a7703d49ff3
mbrown <tugtuj@gmail.com>
2021-02-12 21:55:30 -0500

    Test Type Func

---
+++ js/loop.js
@@ -0,0 +1,22 @@
+
+const sep = '***************************'
+
+const typeFunc = function(words){
+  
+      setInterval(function(){
+          for (let i = 0; i < words.length; i++) {
+          console.log(sep)
+          //console.log(words)
+          console.log(words[i])
+          }
+         // console.log(wordsTyped)
+      }, 1500);
+     // console.log(words[i])
+  
+  //console.log(sep)
+  //console.log(words)
+  //console.log(words[i])
+}
+
+  
+typeFunc('Hello there!')
